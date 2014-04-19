dbebaf5c0dd18492c1880ee5d68d998a1452cc17
George Marshall <george@georgemarshall.name>
2014-04-19 07:05:41 -0700

    Updated pillow formula dependencies

--- pillow.rb
+++ pillow.rb
@@ -8,19 +8,17 @@ class Pillow < Formula
 
   depends_on :python => :recommended
   depends_on :python3 => :optional
-  depends_on 'little-cms'
-  depends_on 'graphicsmagick'
   depends_on 'freetype'
   depends_on 'jpeg'
   depends_on 'libtiff'
+  depends_on 'little-cms2'
+  depends_on 'openjpeg'
+  depends_on 'webp'
 
   def install
-    # Help pillow find zlib and little-cms (Note freetype2 is detected correctly)
+    # Help pillow find zlib and freetype2
     inreplace "setup.py" do |s|
       s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{MacOS.sdk_path}/usr/lib', '#{MacOS.sdk_path}/usr/include')" unless MacOS::CLT.installed?
-      s.gsub! "LCMS_ROOT = None", "LCMS_ROOT = ('#{Formula["littlecms"].opt_prefix}/lib', '#{Formula["littlecms"].opt_prefix}/include')"
-      s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
-      s.gsub! "TIFF_ROOT = None", "TIFF_ROOT = ('#{Formula["libtiff"].opt_prefix}/lib', '#{Formula["libtiff"].opt_prefix}/include')"
       s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
     end
 
